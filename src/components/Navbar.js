f4120cc790d4091474fed5cfdeaa9999ed629ce5
Sumit Tuladhar <sumittuladhar@gmail.com>
2019-06-22 21:34:08 +0545

    Accessing context using Consumer method

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,24 +2,27 @@ import React, {Component} from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 export class Navbar extends Component {
-  static contextType = ThemeContext;
+  // static contextType = ThemeContext;
   render() {
 
     console.log(this.context);
-    const {isLightTheme, light, dark} = this.context;
-    const theme = isLightTheme ? light: dark;
+    
 
     return (
-      <div>
-        <nav style={{background: theme.ui, color: theme.syntax}}>
-          <h1>Context App</h1>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
-      </div>
+      <ThemeContext.Consumer>{(context)=>{
+        const { isLightTheme, light, dark } = context;
+        const theme = isLightTheme ? light : dark;
+          return (
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
+              <h1>Context App</h1>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          );
+        }}</ThemeContext.Consumer>
     )
   }
 }
